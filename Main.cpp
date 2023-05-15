1822ca0cd868aace5135d80d026af0858d9bccc1
Johanna Wirell <jw223gx@student.lnu.se>
2023-05-15 15:51:50 +0200

    Initial setup

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "game/Game.h"
+
+int main() {
+    Game game;
+    game.start();
+
+    return 0;
+}
---
+++ game/Game.cpp
@@ -0,0 +1,10 @@
+#include "Game.h"
+#include <cstdio>
+
+Game::Game() {
+    
+}
+
+void Game::start() {
+    printf("Start Flappy Bird!\n");
+}
